f4ce6b9d0b0d1054d1371bcda8dcc81eca0064be
Debanjan Dutta <149999574+Debanjan110d@users.noreply.github.com>
2025-03-27 10:10:15 +0000

    some changes in code in online browser

--- chaiorcode/chaiorcode/settings.py
+++ chaiorcode/chaiorcode/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chai',
+    'tailwind',
+    'theme',
+  
 ]
 
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    '127.0.0.1',  # Localhost
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
